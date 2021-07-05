b3bd6c70ba88a4ff5df2b683f11020d5ff437beb
woutdenolf <woutdenolf@users.sf.net>
2021-07-05 22:31:51 +0200

    Fix

--- ewoksorange/tests/test_orange_job.py
+++ ewoksorange/tests/test_orange_job.py
@@ -11,7 +11,7 @@ logging.getLogger("ewoksorange").setLevel(logging.DEBUG)
 logging.getLogger("ewoksorange").addHandler(logging.StreamHandler(sys.stdout))
 
 
-@pytest.skip("TODO: hashes are different due to static input")
+@pytest.mark.skip("TODO: hashes are different due to static input")
 def test_job(tmpdir):
     varinfo = {"root_uri": str(tmpdir)}
     graph, expected = graphs.acyclic_graph1()
