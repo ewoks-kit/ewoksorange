bb32de6f77b364661bdc6ceb45190b9a03548362
woutdenolf <woutdenolf@users.sf.net>
2021-09-20 11:13:07 +0200

    allow running an orange workflow without GUI and without converting it to ewoks

--- ewoksorange/tests/conftest.py
+++ ewoksorange/tests/conftest.py
@@ -1,8 +1,14 @@
+import logging
 import pytest
 from ewoksorange.registration import register_addon_package
-from ewoksorange.bindings.qtapp import ensure_qtapp
+from ewoksorange.bindings.qtapp import qtapp_context
+from ewoksorange.bindings.qtapp import get_all_qtwidgets
+from ewoksorange.canvas.handler import OrangeCanvasHandler
 from .examples import ewoks_example_1_addon
 from .examples import ewoks_example_2_addon
+
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="session")
@@ -26,6 +32,22 @@ def register_ewoks_example_addons(
 
 @pytest.fixture(scope="session")
 def qtapp():
-    APP = ensure_qtapp()
-    yield
-    APP.exit()
+    with qtapp_context() as app:
+        yield app
+    warn_qtwidgets_alive()
+
+
+@pytest.fixture(scope="session")
+def ewoks_orange_canvas(qtapp, register_ewoks_example_addons):
+    with OrangeCanvasHandler() as handler:
+        yield handler
+
+
+def warn_qtwidgets_alive():
+    widgets = get_all_qtwidgets()
+    if widgets:
+        logger.warning(
+            "%d remaining widgets after tests:\n %s",
+            len(widgets),
+            "\n ".join(map(str, widgets)),
+        )
--- ewoksorange/tests/test_examples.py
+++ ewoksorange/tests/test_examples.py
@@ -1,10 +1,13 @@
+import os
 import sys
 import logging
+import tempfile
 import pytest
-from ewoksorange import execute_graph
+
 from ewokscore.tests.examples.graphs import get_graph
 from ewokscore.tests.utils import assert_taskgraph_result
 from ewokscore import load_graph
+from ewoksorange.bindings import ewoks_to_ows
 
 logging.getLogger("orange").setLevel(logging.DEBUG)
 logging.getLogger("orange").addHandler(logging.StreamHandler(sys.stdout))
@@ -13,14 +16,20 @@ logging.getLogger("ewoksorange").addHandler(logging.StreamHandler(sys.stdout))
 
 
 @pytest.mark.parametrize("graph_name", ["acyclic1", "cyclic1"])
-def test_execute_graph(graph_name, tmpdir, register_ewoks_example_addons):
+def test_execute_graph(graph_name, tmpdir, ewoks_orange_canvas):
+    """Test graph execution like the Orange canvas would do it"""
     graph, expected = get_graph(graph_name)
     ewoksgraph = load_graph(graph)
     varinfo = {"root_uri": str(tmpdir)}
     if ewoksgraph.is_cyclic or ewoksgraph.has_conditional_links:
-        with pytest.raises(RuntimeError):
-            execute_graph(graph, varinfo=varinfo)
+        pytest.skip("graph not supported by orange")
     else:
-        pytest.skip("Not sure how to close the Qt app when everything is done")
-        execute_graph(graph, varinfo=varinfo)
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            filename = os.path.join(tmpdirname, graph_name + ".ows")
+            ewoks_to_ows(
+                ewoksgraph, filename, varinfo=varinfo, error_on_duplicates=False
+            )
+            ewoks_orange_canvas.load_ows(filename)
+        ewoks_orange_canvas.wait_widgets(timeout=60)
+
         assert_taskgraph_result(ewoksgraph, expected, varinfo=varinfo)
