9ac113d57bab51506ae4877532ad7e770cbdccf6
woutdenolf <woutdenolf@users.sf.net>
2021-12-20 16:34:17 +0100

    refactor tests

--- ewoksorange/tests/test_examples.py
+++ ewoksorange/tests/test_examples.py
@@ -1,21 +1,15 @@
 import os
-import sys
-import logging
 import tempfile
 import pytest
 
+from ewokscore.tests.examples.graphs import graph_names
 from ewokscore.tests.examples.graphs import get_graph
-from ewokscore.tests.utils import assert_taskgraph_result
+from ewokscore.tests.utils.results import assert_execute_graph_all_tasks
 from ewokscore import load_graph
 from ewoksorange.bindings import ewoks_to_ows
 
-logging.getLogger("orange").setLevel(logging.DEBUG)
-logging.getLogger("orange").addHandler(logging.StreamHandler(sys.stdout))
-logging.getLogger("ewoksorange").setLevel(logging.DEBUG)
-logging.getLogger("ewoksorange").addHandler(logging.StreamHandler(sys.stdout))
 
-
-@pytest.mark.parametrize("graph_name", ["acyclic1", "cyclic1"])
+@pytest.mark.parametrize("graph_name", graph_names())
 def test_execute_graph(graph_name, tmpdir, ewoks_orange_canvas):
     """Test graph execution like the Orange canvas would do it"""
     graph, expected = get_graph(graph_name)
@@ -31,13 +25,11 @@ def test_execute_graph(graph_name, tmpdir, ewoks_orange_canvas):
         or no_explicit_datamapping
     ):
         pytest.skip("graph not supported by orange")
-    else:
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            filename = os.path.join(tmpdirname, graph_name + ".ows")
-            ewoks_to_ows(
-                ewoksgraph, filename, varinfo=varinfo, error_on_duplicates=False
-            )
-            ewoks_orange_canvas.load_ows(filename)
-        ewoks_orange_canvas.wait_widgets(timeout=10)
 
-        assert_taskgraph_result(ewoksgraph, expected, varinfo=varinfo)
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        filename = os.path.join(tmpdirname, graph_name + ".ows")
+        ewoks_to_ows(ewoksgraph, filename, varinfo=varinfo, error_on_duplicates=False)
+        ewoks_orange_canvas.load_ows(filename)
+    ewoks_orange_canvas.wait_widgets(timeout=10)
+
+    assert_execute_graph_all_tasks(ewoksgraph, expected, varinfo=varinfo)
