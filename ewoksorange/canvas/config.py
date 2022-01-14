832d8ae4f1e0da45a5e8ac13d07a6554e4307dac
woutdenolf <woutdenolf@users.sf.net>
2022-01-14 11:38:10 +0100

    missing method

--- ewoksorange/canvas/config.py
+++ ewoksorange/canvas/config.py
@@ -1,6 +1,9 @@
-from orangewidget.workflow import config as orangeconfig
-from ewoksorange.registration import NATIVE_WIDGETS_PROJECT
+"""Copy parts of Orange.canvas.config to be used when Orange3 is not installed.
+"""
+
 import pkg_resources
+from orangewidget.workflow import config as orangeconfig
+from orangewidget.workflow.config import WIDGETS_ENTRY
 
 
 class Config(orangeconfig.Config):
@@ -12,9 +15,13 @@ class Config(orangeconfig.Config):
         """
         # Ensure the 'this' distribution's ep is the first. iter_entry_points
         # yields them in unspecified order.
-        all_eps = pkg_resources.iter_entry_points(NATIVE_WIDGETS_PROJECT + ".widgets")
+        all_eps = pkg_resources.iter_entry_points(WIDGETS_ENTRY)
         return iter(all_eps)
 
     @staticmethod
     def addon_entry_points():
         return Config.widgets_entry_points()
+
+
+def widgets_entry_points():
+    return Config.widgets_entry_points()
