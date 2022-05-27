fba45fdc51a48b6e8de2c419b2effa3c1e165203
woutdenolf <woutdenolf@users.sf.net>
2022-05-27 14:45:13 +0200

    use orange_imports in adder example widgets

--- ewoksorange/gui/orange_imports.py
+++ ewoksorange/gui/orange_imports.py
@@ -5,8 +5,10 @@ from ..orange_version import ORANGE_VERSION
 
 if ORANGE_VERSION == ORANGE_VERSION.oasys_fork:
     from oasys.widgets import gui  # noqa F401
-    from oasys.widgets.settings import Setting  # noqa F401
     from oasyswidget.settings import Setting  # noqa F401
+
+    Input = None
+    Output = None
 elif ORANGE_VERSION == ORANGE_VERSION.latest_orange:
     from Orange.widgets import gui  # noqa F401
     from Orange.widgets.widget import Input, Output  # noqa F401
