d02036e0e1609d971840e3191e635502306fdb98
Henri Payno <henri.payno@gmail.com>
2022-08-04 14:21:37 +0200

    improve progress bar management

--- src/ewoksorange/bindings/owwidgets.py
+++ src/ewoksorange/bindings/owwidgets.py
@@ -424,9 +424,11 @@ class OWEwoksWidgetNoThread(OWEwoksBaseWidget, **ow_build_opts):
 class _OWEwoksThreadedBaseWidget(OWEwoksBaseWidget, **ow_build_opts):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.__taskProgress = QProgress()
         if has_progress_bar:
+            self.__taskProgress = QProgress()
             self.__taskProgress.sigProgressChanged.connect(self.progressBarSet)
+        else:
+            self.__taskProgress = None
 
     def onDeleteWidget(self):
         if has_progress_bar:
@@ -454,10 +456,12 @@ class _OWEwoksThreadedBaseWidget(OWEwoksBaseWidget, **ow_build_opts):
             self.__ewoks_task_finished()
 
     def __ewoks_task_init(self):
-        self.progressBarInit()
+        if has_progress_bar:
+            self.progressBarInit()
 
     def __ewoks_task_finished(self):
-        self.progressBarFinished()
+        if has_progress_bar:
+            self.progressBarFinished()
         self._output_changed()
 
     def _get_task_arguments(self):
