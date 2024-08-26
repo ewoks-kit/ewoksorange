1b44aa69f459ebb23ab8d3bb434d8656ed4e5a59
payno <henri.payno@esrf.fr>
2024-08-26 13:07:12 +0200

    fix OWEwoksWidgetOneThreadPerRun

--- src/ewoksorange/bindings/owwidgets.py
+++ src/ewoksorange/bindings/owwidgets.py
@@ -643,7 +643,7 @@ class OWEwoksWidgetOneThreadPerRun(_OWEwoksThreadedBaseWidget, **ow_build_opts):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.__task_executors = dict()
+        self.__task_executors: dict[int, ThreadedTaskExecutor] = dict()
         self.__last_output_variables = dict()
         self.__last_task_succeeded = None
         self.__last_task_done = None
@@ -698,7 +698,7 @@ class OWEwoksWidgetOneThreadPerRun(_OWEwoksThreadedBaseWidget, **ow_build_opts):
 
     def _cleanup_task_executor(self):
         self.__disconnect_all_task_executors()
-        for task_executor, _ in self.__task_executors:
+        for task_executor in self.__task_executors.values():
             task_executor.quit()
         self.__task_executors.clear()
 
