62000d5befd0f24f631c02d4ce361b0267372195
payno <henri.payno@gmail.com>
2025-08-13 19:12:27 +0200

    'can_enable_dynamic' monkey patch: also do it for oasys.

--- src/ewoksorange/bindings/owsignal_manager.py
+++ src/ewoksorange/bindings/owsignal_manager.py
@@ -13,17 +13,14 @@ if ORANGE_VERSION == ORANGE_VERSION.oasys_fork:
             return bool(self._input_queue)
 
     notify_input_helper = None
-    monkey_patch_can_enable_dynamic = False
 else:
-    import orangecanvas.scheme.signalmanager
-
     from orangewidget.workflow.widgetsscheme import (
         WidgetsSignalManager as _SignalManagerWithScheme,
     )
     import orangewidget.workflow.widgetsscheme as widgetsscheme_module
     from orangewidget.utils.signals import notify_input_helper
 
-    monkey_patch_can_enable_dynamic = True
+from orangecanvas.scheme import signalmanager
 
 from ewokscore.variable import value_from_transfer, Variable
 
@@ -33,22 +30,22 @@ from ..bindings.owsignals import get_input_names, get_output_names
 from . import invalid_data
 
 
-if monkey_patch_can_enable_dynamic:
-    # monkey patch of 'can_enable_dynamic' See https://gitlab.esrf.fr/workflow/ewoks/ewoksorange/-/issues/58
+# monkey patch of 'can_enable_dynamic' See https://gitlab.esrf.fr/workflow/ewoks/ewoksorange/-/issues/58
 
-    _super_can_enable_dynamic = orangecanvas.scheme.signalmanager.can_enable_dynamic
-
-    def can_enable_dynamic_patch(link, value):
-        # type: (SchemeLink, Any) -> bool
-        """
-        Can the a dynamic `link` (:class:`SchemeLink`) be enabled for`value`.
-        """
-        if isinstance(value, Variable):
-            value = value.value
-        return _super_can_enable_dynamic(link, value)
+_super_can_enable_dynamic = signalmanager.can_enable_dynamic
 
 
-orangecanvas.scheme.signalmanager.can_enable_dynamic = can_enable_dynamic_patch
+def can_enable_dynamic_patch(link, value):
+    # type: (SchemeLink, Any) -> bool
+    """
+    Can the a dynamic `link` (:class:`SchemeLink`) be enabled for`value`.
+    """
+    if isinstance(value, Variable):
+        value = value.value
+    return _super_can_enable_dynamic(link, value)
+
+
+signalmanager.can_enable_dynamic = can_enable_dynamic_patch
 
 # en monkey patch
 
