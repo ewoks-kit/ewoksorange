d52c9338fa9126fd93657f4baf51f7fec08d4c26
woutdenolf <woutdenolf@users.sf.net>
2025-11-21 13:43:57 +0100

    release 3.0.1

--- src/ewoksorange/gui/parameterform.py
+++ src/ewoksorange/gui/parameterform.py
@@ -1,6 +1,7 @@
 import warnings
 
 from .widgets.parameter_form import ParameterForm  # noqa F401
+from .widgets.parameter_form import block_signals  # noqa F401
 
 warnings.warn(
     f"The '{__name__}' module is deprecated and will be removed in a future release. "
