89f6fcba075f18499a4d9532a1cb8afba0c9f194
Marius Retegan <marius.s.retegan@gmail.com>
2026-04-13 14:13:32 +0200

    Add `--with-demo` flag

--- src/orangecontrib/ewoksdemo/__init__.py
+++ src/orangecontrib/ewoksdemo/__init__.py
@@ -2,6 +2,27 @@ import sysconfig
 
 NAME = "Ewoks Demo"
 
+
+def widget_discovery(_discovery):
+    """Do not show any widgets by default."""
+    pass
+
+
+def enable_ewoksdemo_category():
+    global widget_discovery
+    try:
+        del widget_discovery
+    except NameError:
+        pass
+
+
+def is_ewoksdemo_category_enabled() -> bool:
+    try:
+        widget_discovery(None)
+    except NameError:
+        return True
+    return False
+
 DESCRIPTION = "Ewoks Demo"
 
 LONG_DESCRIPTION = "Ewoks Demo"
