29a02bf7b8dc71c7e8919c713f076e8cb725d2d6
payno <henri.payno@gmail.com>
2026-05-21 08:24:34 +0200

    CanvasMainWindow: add comment

--- src/ewoksorange/gui/canvas/mainwindow.py
+++ src/ewoksorange/gui/canvas/mainwindow.py
@@ -5,3 +5,4 @@ from .schemeedit import SchemeEditWidget
 class CanvasMainWindow(_CanvasMainWindow):
 
     EDITOR_WIDGET_CONSTRUCTOR = SchemeEditWidget
+    # Redefining the 'EDITOR_WIDGET_CONSTRUCTOR' allow use to add features to the default 'SchemeEditWidget' like adding actions to the link.
