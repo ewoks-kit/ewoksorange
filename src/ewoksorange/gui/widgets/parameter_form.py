b0f071e898ebcf6ef597745d7245070e2351c114
ruyer <maxence.ruyer@esrf.fr>
2026-06-02 10:35:24 +0200

    feat[ParameterForm]: #36 Add an auto-completer for file / directory selection

--- src/ewoksorange/gui/widgets/parameter_form.py
+++ src/ewoksorange/gui/widgets/parameter_form.py
@@ -12,6 +12,7 @@ from typing import Set
 from typing import Union
 
 from AnyQt import QtCore
+from AnyQt import QtGui
 from AnyQt import QtWidgets
 from ewokscore import missing_data
 from silx.gui.dialog.DataFileDialog import DataFileDialog
@@ -287,6 +288,15 @@ class ParameterForm(QtWidgets.QWidget):
                 )
             else:
                 select_widget = None
+            if select in (SelectMode.DIRECTORY, SelectMode.DIRECTORIES):
+                self._attach_path_completer(value_widget, dir_only=True)
+            elif select in (
+                SelectMode.FILE,
+                SelectMode.NEW_FILE,
+                SelectMode.FILES,
+                SelectMode.NEW_FILES,
+            ):
+                self._attach_path_completer(value_widget, dir_only=False)
         elif isinstance(value_for_type, bool):
             value_widget = QtWidgets.QCheckBox(bool_label)
             if value_change_callback:
@@ -702,6 +712,16 @@ class ParameterForm(QtWidgets.QWidget):
             return lst[-1]
         else:
             raise TypeError(lst)
+
+    def _attach_path_completer(
+        self, widget: QtWidgets.QLineEdit, dir_only: bool = False
+    ) -> None:
+        completer = QtWidgets.QCompleter(widget)
+        model = QtGui.QFileSystemModel(completer)
+        if dir_only:
+            model.setFilter(QtCore.QDir.AllDirs | QtCore.QDir.NoDotAndDotDot)
+        completer.setModel(model)
+        widget.setCompleter(completer)
 
     def keyPressEvent(self, event):
         key = event.key()
