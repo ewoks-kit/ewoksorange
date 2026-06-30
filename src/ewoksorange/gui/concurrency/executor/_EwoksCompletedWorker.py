03fbea09f6288360b1d394dff3ac86970625ece4
payno <henri.payno@gmail.com>
2026-06-30 13:48:58 +0200

    CompletedWorker: improve docstring.

--- src/ewoksorange/gui/concurrency/executor/_EwoksCompletedWorker.py
+++ src/ewoksorange/gui/concurrency/executor/_EwoksCompletedWorker.py
@@ -2,6 +2,7 @@ class CompletedWorker:
     """No-op worker for an already-finished synchronous task."""
 
     def abort(self) -> None:
+        # no abortion possible. We expect the Worker to be created once the task is completed.
         pass
 
     @property
