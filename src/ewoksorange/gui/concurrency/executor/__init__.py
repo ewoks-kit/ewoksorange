e6ec06c6000286e0d648b5a8be120c466bba5fce
payno <henri.payno@gmail.com>
2026-06-30 08:48:48 +0200

    ewoksorange.gui.concurrency.executor: add docstring for the module

--- src/ewoksorange/gui/concurrency/executor/__init__.py
+++ src/ewoksorange/gui/concurrency/executor/__init__.py
@@ -1,3 +1,8 @@
+"""
+Qt-aware executor layer for running Ewoks tasks in background threads or
+subprocesses.
+"""
+
 from .EwoksExecutor import EwoksExecutor  # noqa F401
 from .EwoksExecutor import SubmitPolicy  # noqa F401
 from .TaskFuture import TaskFuture  # noqa F401
