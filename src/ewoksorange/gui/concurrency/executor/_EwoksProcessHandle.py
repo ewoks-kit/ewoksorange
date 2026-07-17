fefe19c4a63c4ddd27d4465fa68cb085e4cfd693
payno <henri.payno@gmail.com>
2026-07-17 15:27:00 +0200

    EwoksProcessWorker: add typing
    # Conflicts:
    #	src/ewoksorange/gui/concurrency/executor/_EwoksProcessWorker.py
    
    # Conflicts:
    #	src/ewoksorange/gui/concurrency/executor/_EwoksProcessWorker.py
    
    # Conflicts:
    #	src/ewoksorange/gui/concurrency/executor/_EwoksProcessHandle.py

--- src/ewoksorange/gui/concurrency/executor/_EwoksProcessHandle.py
+++ src/ewoksorange/gui/concurrency/executor/_EwoksProcessHandle.py
@@ -1,10 +1,11 @@
+import multiprocessing
 from ._EwoksTaskHandle import EwoksTaskHandle
 
 
 class EwoksProcessHandle(EwoksTaskHandle):
     """Controls a task running in a subprocess via multiprocessing.Event objects."""
 
-    def __init__(self, abort_event, aborted_event):
+    def __init__(self, abort_event: multiprocessing.Event, aborted_event: multiprocessing.Event):  # type: ignore
         self._abort_event = abort_event
         self._aborted_event = aborted_event
 
