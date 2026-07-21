f9bc006a80561f0137aa7b2047966c8e870079d1
payno <henri.payno@gmail.com>
2026-07-21 11:07:56 +0200

    _OWEwoksExecutorWidget: improve warning message (thanks to Loic ;) ).

--- src/ewoksorange/gui/owwidgets/threaded.py
+++ src/ewoksorange/gui/owwidgets/threaded.py
@@ -136,7 +136,7 @@ class _OWEwoksExecutorWidget(_OWEwoksThreadedBaseWidget, **ow_build_opts):
     def cancel_running_task(self) -> None:
         """Abort the currently running task."""
         warnings.warn(
-            "'cancel_running_task' is deprecated since 6.0 Please use directly the future provided during task submission.",
+            "'cancel_running_task' is deprecated since 6.0. Please cancel the task by calling the  `cancel` method of the future provided during task submission.",
             DeprecationWarning,
         )
         if self.__current_task_future is not None:
