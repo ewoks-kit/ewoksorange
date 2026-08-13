16700079edca675e2c88059eea249f6096448e34
woutdenolf <woutdenolf@users.sf.net>
2026-08-13 15:14:42 +0200

    optimization: skip widget lookup when not needed

--- src/ewoksorange/bindings/__init__.py
+++ src/ewoksorange/bindings/__init__.py
@@ -127,8 +127,12 @@ def _get_output_values(
     merge_outputs: Optional[bool] = True,
 ) -> Dict:
     parsed_outputs = graph_io.parse_outputs(graph, outputs)
+    output_node_ids = {item["id"] for item in parsed_outputs}
     output_values: Dict = dict()
     for node_id in networkx.topological_sort(graph):
+        if node_id not in output_node_ids:
+            # optimization: skip widget lookup
+            continue
         widget = handler.widget_from_id(node_id)
         task_output_values = widget.get_task_output_values()
         graph_io.add_output_values(
